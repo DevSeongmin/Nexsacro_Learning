f9b18600c28c2cdf7f0287f8ac3d7c98d61bf648
SeongMin Hwang <qq221qq@naver.com>
2024-10-26 14:31:04 +0900

    스프링 서버 생성

---
+++ NexacroServer/src/main/java/com/jb/nexacroserver/NexacroServerApplication.java
@@ -0,0 +1,13 @@
+package com.jb.nexacroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexacroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexacroServerApplication.class, args);
+	}
+
+}
---
+++ NexacroServer/src/test/java/com/jb/nexacroserver/NexacroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.nexacroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexacroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
